944bfa287119619283651fd289aa4c37e35a6588
finleygn <finleygarton12@gmail.com>
2025-03-22 16:53:41 +0000

    remove cursor on touch devices

--- src/components/DogCursor.tsx
+++ src/components/DogCursor.tsx
@@ -12,6 +12,7 @@ function DogCursor(props: DogCursorProps) {
   let cursorRef: HTMLImageElement | undefined;
 
   const [hasMoved, setHasMoved] = createSignal(false);
+  const [disabled, setDisabled] = createSignal(false);
 
   const onMouseMove = (mouseEvent: MouseEvent) => {
     if(!cursorRef) return;
@@ -19,8 +20,12 @@ function DogCursor(props: DogCursorProps) {
     cursorRef.style.left = `${mouseEvent.clientX}px`;
     cursorRef.style.top = `${mouseEvent.clientY}px`;
   };
+  const disableOnTouch = () => {
+    setDisabled(true);
+  };
 
   onMount(() => {
+    window.addEventListener('touchstart', disableOnTouch);
     window.addEventListener('mousemove', onMouseMove)
   });
 
@@ -32,7 +37,7 @@ function DogCursor(props: DogCursorProps) {
     <Portal>
       <div ref={cursorRef} classList={{
         cursor: true,
-        'cursor--enabled': props.enabled && hasMoved()
+        'cursor--enabled': !disabled() && props.enabled && hasMoved()
       }}>
         <img src={props.default.src} class='cursor__image cursor__image--default' />
         <img src={props.hovered.src} class='cursor__image cursor__image--hovered' />
