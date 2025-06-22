6d2cea1401d47d76eff775f9b53489e8de539071
finleygn <finleygarton12@gmail.com>
2025-06-22 16:33:56 +0100

    update

--- src/screens/Home/components/Icon.tsx
+++ src/screens/Home/components/Icon.tsx
@@ -20,9 +20,9 @@ function Icon({ title, thumbnail, id, position, available, ref }: IconProps) {
       ref={ref}
       style={{ top: `${position.y}%`, left: `${position.x}%` }}
     >
-      <img src={thumbnail} draggable={false} alt={`${title} thumbnail`} />
+      <img src={thumbnail} draggable={false} alt={`${title} thumbnail`} class="artwork-icon__thumbnail" />
       <span class="artwork-icon__text">{title}</span>
-      {available && <img src="/meow/prints-available.svg" />}
+      {available && <img src="/meow/prints-available.svg" class="artwork-icon__badge" />}
     </A>
   );
 }
